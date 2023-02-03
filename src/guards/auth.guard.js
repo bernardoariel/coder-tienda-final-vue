4ff6b9ab184e6ce2f701000aa8ff474696aca5c0
ariel Bernardo <arielbernardo@hotmail.com>
2023-02-03 07:52:32 -0300

    Agregando readme

--- src/guards/auth.guard.js
+++ src/guards/auth.guard.js
@@ -1,11 +1,15 @@
 import { useUsuarioStore } from "../stores/usuarioStore";
 
-const estaAutenticado = async (to,from,next) =>{
+const estaAutenticado =  (to,from,next) =>{
+
     const usuarioStore = useUsuarioStore();
+
     if(usuarioStore.tipo!=undefined && usuarioStore.tipo == 'admin'){
 
         next()
+
     }else{
+
         next({path:'/'})
     }
 }
